46820ca085d47a59ae228a477e45bfd049c30a72
Jasmine Hollis <jasmine.hollis@rocketmail.com>
2018-04-12 15:51:14 -0400

    First commit for Math Challenge

---
+++ Lab1_MathChallenge/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Lab1_MathChallenge
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("\n\n Game to Calculate Corresponding Space of 2 Integers :\n");
+            Console.Write("——————————————————————————————————————————————————————————————\n");
+            Console.Write("Enter first 3-digit number: ");
+
+            String firstInput = Console.ReadLine();
+
+            //Validates user input and notifies user
+            while (firstInput.Length != 3 || !Int32.TryParse(firstInput, out int firstOutput))
+            {
+                Console.WriteLine("We need a 3-digit number, please try again.");
+                firstInput = Console.ReadLine();
+            }
+            int num1 = Convert.ToInt32(firstInput);
+
+
+            Console.Write("Enter second 3-digit number: ");
+            String secondInput = Console.ReadLine();
+
+            while (secondInput.Length != 3 || !Int32.TryParse(secondInput, out int secondOutput))
+            {
+                Console.WriteLine("We need a 3-digit number, please try again.");
+                secondInput = Console.ReadLine();
+            }
+            int num2 = Convert.ToInt32(secondInput);
+
+            // 
+            int firstNum1, secondNum1, thirdNum1 = 0;
+            int firstNum2, secondNum2, thirdNum2 = 0;
+
+            //Dividing integers for comparison
+            firstNum1 = (num1 % 10);
+            secondNum1 = (num1 / 10) % 10;
+            thirdNum1 = (num1 / 100);
+
+            firstNum2 = (num2 % 10);
+            secondNum2 = (num2 / 10) % 10;
+            thirdNum2 = (num2 / 100);
+
+
+            int total1 = 0;
+            int total2 = 0;
+            int total3 = 0;
+
+            //adding corresponding numbers
+            total1 = (thirdNum1 + thirdNum2);
+            total2 = (secondNum1 + secondNum2);
+            total3 = (firstNum1 + firstNum2);
+
+            //determines if corresponding numbers are equal
+            bool a = (total1 == total2 && total2 == total3);
+
+            Console.WriteLine("We added the numbers and here are the results!: ("
+                              + total1 + "," + total2 + "," + total3 + ") which is " + a);
+
+        }
+    }
+}
